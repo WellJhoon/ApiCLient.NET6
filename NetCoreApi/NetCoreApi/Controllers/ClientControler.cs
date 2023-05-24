24348aef23624ffc43ffc32a5b09ac5982c544d8
WellJhon <75251753+WellJhoon@users.noreply.github.com>
2023-05-23 21:45:32 -0400

    Add files via upload

---
+++ NetCoreApi/NetCoreApi/Controllers/ClientControler.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreApi.Models;
+
+namespace NetCoreApi.Controllers
+{
+    [ApiController]
+    [Route("clients")]
+    public class ClientControler : ControllerBase
+    {
+        [HttpGet]
+        [Route("Listar")]
+        public dynamic listClients()
+        {
+            List<Client> client = new List<Client>
+            {
+                new Client
+                {
+                  id = "1",
+                  name = "Jhon",
+                  edad = "20",
+                  correo = "Jhon43769@gmail.com"
+                },
+                  new Client
+                {
+                  id = "2",
+                  name = "pedro",
+                  edad = "20",
+                  correo = "pedro@gmail.com"
+                }
+            };
+            return client;
+        }
+
+        [HttpGet]
+        [Route("ListarPorId")]
+        public dynamic listClientsforId(int codigo)
+        {
+            //Obtencion de cliente de la Base de datos
+            return new Client
+            {
+                id = codigo.ToString(),
+                name = "test",
+                edad = "52",
+                correo = "pepebotella@gmail.com"
+            };
+        }
+
+
+        [HttpPost]
+        [Route("save")]
+        public dynamic saveClients(Client client)
+        {
+            client.id = "3";
+
+            return new
+            {
+                succes = true,
+                message = "Cliente registrado",
+                Results = client
+            };
+        }
+
+        [HttpPost]
+        [Route("Delete")]
+        public dynamic DeleteClient(Client client)
+        {
+            string token = Request.Headers.Where(x => x.Key == "Authorization").FirstOrDefault().Value;
+
+            if(token != "Admin123.")
+            {
+                return new
+                {
+                    succes = false,
+                    message = "Token Incorrento",
+                    Results = client
+                };
+            }
+            
+            return new
+            {
+                succes = true,
+                message = "Cliente Eliminado",
+                Results = client
+            };
+        }
+
+    }
+}
---
+++ NetCoreApi/NetCoreApi/Models/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace NetCoreApi.Models
+{
+    public class Client
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string edad { get; set; }
+        public string correo { get; set; }
+
+    }
+}
